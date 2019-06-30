a12d0b453391e2f4661f033bdc8e44cf0f9807ee
boolfalse <boolfalse@mail.ru>
2019-07-01 01:25:17 +0400

    Moved User-specific controllers to the 'app/Http/Controllers/User/' (routes are changed)

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return view('home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+})->name('front');
+
+Route::group([
+    'namespace' => 'User\\Auth',
+], function () {
+
+    // Authentication Routes...
+    Route::get('login', 'LoginController@showLoginForm')->name('login_page');
+    Route::post('login', 'LoginController@login')->name('login');
+    Route::post('logout', 'LoginController@logout')->name('logout');
+
+    // Registration Routes...
+    Route::get('register', 'RegisterController@showRegistrationForm')->name('register_page');
+    Route::post('register', 'RegisterController@register')->name('register');
+    Route::get('register/activate/{token}', 'RegisterController@confirm')->name('email_confirm');
+
+    // Password Reset Routes...
+    Route::get('password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'ResetPasswordController@reset');
+
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('home', 'User\\UserController@index')->name('home');
