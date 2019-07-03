2d479303e7e9e74de8a7c19a7b714415d24e7efb
boolfalse <boolfalse@mail.ru>
2019-07-03 08:37:29 +0400

    new configs; 'cleanuploads' command

---
+++ app/Console/Commands/CleanUploads.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+
+class CleanUploads extends Command
+{
+    protected $signature = 'cleanuploads';
+
+    protected $description = 'Clean uploaded files.';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $file = new Filesystem;
+
+        // delete uploads from public
+        $directory = 'storage' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . config('project.user.images_folder');
+        $file->cleanDirectory($directory);
+
+        // delete uploads from storage
+        foreach (config('project.user.image_sizes') as $version => $sizes){
+            $directory = 'public' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . config('project.user.images_folder') . DIRECTORY_SEPARATOR . $version;
+            $file->cleanDirectory($directory);
+        }
+
+        echo "Uploads successfully deleted!\n";
+    }
+}
--- config/project.php
+++ config/project.php
@@ -5,6 +5,38 @@ return [
         'dev_name' => env('DEV_NAME', 'test'),
         'dev_email' => env('DEV_EMAIL', 'test@gmail.com'),
         'dev_password' => env('DEV_PASSWORD', 'secret'),
+
+        'users_count' => 5,
+        'user_faker_image_width' => 306, // 1400,
+        'user_faker_image_height' => 440, // 1000,
+    ],
+    'user' => [
+        'images_folder' => 'user-images',
+        // width, height, padding-bottom, padding-right
+        'image_sizes' => [
+            "small" => [
+                "w" => 120, // 70
+                "h" => 195, // 70
+                "pb" => 3,
+                "pr" => 3,
+            ],
+            "medium" => [
+                "w" => 160, // 370,
+                "h" => 260, // 200,
+                "pb" => 5,
+                "pr" => 5,
+            ],
+            "large" => [
+                "w" => 306, // 1140,
+                "h" => 440, // 642,
+                "pb" => 10,
+                "pr" => 10,
+            ],
+        ],
+        'image_divisions' => [
+            'min' => 1.2,
+            'max' => 2,
+        ],
     ],
     'admin' => [
         'prefix' => env('ADMIN_PREFIX', 'admin'),
