9d96b2b7f56ba921ee3125b697353ca6cef6f0fc
boolfalse <boolfalse@mail.ru>
2019-07-03 08:38:52 +0400

    missed commands kernel file in previous commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,13 +2,14 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CleanUploads;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
-        //
+        CleanUploads::class,
     ];
 
     protected function schedule(Schedule $schedule)
