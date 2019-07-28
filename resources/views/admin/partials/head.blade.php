1f640b1c63485033ac8839b1828208d178634b35
boolfalse <boolfalse@mail.ru>
2019-07-28 15:15:41 +0400

    Removed 'panel-resources' and Replaced with common css|js|images|fonts admin stuff

--- resources/views/admin/partials/head.blade.php
+++ resources/views/admin/partials/head.blade.php
@@ -4,28 +4,19 @@
 <!-- Tell the browser to be responsive to screen width -->
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 <!-- Bootstrap 3.3.7 -->
-<link rel="stylesheet" href="{{ asset('panel-resources/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/admin/bootstrap.min.css') }}">
 <!-- Font Awesome -->
-<link rel="stylesheet" href="{{ asset('panel-resources/bower_components/font-awesome/css/font-awesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/admin/font-awesome.min.css') }}">
 <!-- Ionicons -->
-<link rel="stylesheet" href="{{ asset('panel-resources/bower_components/Ionicons/css/ionicons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/admin/ionicons.min.css') }}">
 <!-- Theme style -->
-<link rel="stylesheet" href="{{ asset('panel-resources/dist/css/BoolFalse.min.css') }}">
-<!-- BoolFalse Skins. Choose a skin from the css/skins
-     folder instead of downloading all of them to reduce the load. -->
-<link rel="stylesheet" href="{{ asset('panel-resources/dist/css/skins/_all-skins.min.css') }}">
-<!-- Morris chart -->
-{{--<link rel="stylesheet" href="{{ asset('panel-resources/bower_components/morris.js/morris.css') }}">--}}
-<!-- jvectormap -->
-{{--<link rel="stylesheet" href="{{ asset('panel-resources/bower_components/jvectormap/jquery-jvectormap.css') }}">--}}
-<!-- Date Picker -->
-{{--<link rel="stylesheet" href="{{ asset('panel-resources/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css') }}">--}}
-<!-- Daterange picker -->
-{{--<link rel="stylesheet" href="{{ asset('panel-resources/bower_components/bootstrap-daterangepicker/daterangepicker.css') }}">--}}
+<link rel="stylesheet" href="{{ asset('css/admin/BoolFalse.min.css') }}">
+<!-- BoolFalse Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
+<link rel="stylesheet" href="{{ asset('css/admin/_all-skins.min.css') }}">
 <!-- bootstrap wysihtml5 - text editor -->
-<link rel="stylesheet" href="{{ asset('panel-resources/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/admin/bootstrap3-wysihtml5.min.css') }}">
 <!-- Select2 -->
-<link rel="stylesheet" href="{{ asset('panel-resources/bower_components/select2/dist/css/select2.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/admin/select2.min.css') }}">
 
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
