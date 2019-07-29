89a7e03ae8f5eef4a5099b2228cb919f052bc02a
boolfalse <boolfalse@mail.ru>
2019-07-29 09:46:32 +0400

    Admin Panel: changed login page (issue fixed)

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,83 +1,67 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>{{ config('app.name') }} | Log in</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('user.layouts.app')
 
-    <!-- Bootstrap 4.0.0 -->
-    <link rel="stylesheet" href="{{ asset('css/admin/bootstrap-4.0.0.min.css') }}">
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Admin {{ __('Login') }}</div>
 
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('admin.login') }}">
+                            @csrf
 
-    <!-- Theme style -->
-    <link href="{{ asset('css/admin/admin.min.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ asset('css/admin/style.css') }}" rel="stylesheet" type="text/css"/>
-</head>
-<body class="login-page" style="background-image: url({{ asset('images/admin.jpg') }})">
-<div class="login-box">
-    <div class="login-logo">
-        <p><b>Multi</b> Auth</p>
-    </div>
-    <div class="login-box-body">
-        <p class="login-box-msg">Sign in to start your session</p>
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-        <form action="{{ route('admin.login') }}" method="post">
-            {{ csrf_field() }}
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-            <div class="form-group has-feedback">
-                <input type="text" name="email" class="form-control" placeholder="Email"/>
-                <i class="fa fa-envelope form-control-feedback"></i>
-                @if ($errors->has('email'))
-                    <span class="invalid-feedback">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
-            </div>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
 
-            <div class="form-group has-feedback">
-                <input type="password" name="password" class="form-control" placeholder="Password"/>
-                <i class="fa fa-lock fa-fw"></i>
-                @if ($errors->has('password'))
-                    <span class="invalid-feedback">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                @endif
-            </div>
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-            <div class="row">
-                <div class="col-8">
-                    <div class="checkbox icheck">
-                        <label>
-                            <input type="checkbox"> Remember Me
-                        </label>
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div class="col-4">
-                    <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-                </div>
             </div>
-        </form>
-
+        </div>
     </div>
-</div>
-
-<!-- Optional JavaScript -->
-<script src="{{ asset('js/admin/jquery-3.3.1.slim.min.js') }}"></script>
-<script src="{{ asset('js/admin/bootstrap-4.1.1.min.js') }}"></script>
-
-<!-- iCheck -->
-<script src="{{ asset('js/admin/admin.min.js') }}" type="text/javascript"></script>
-<script>
-    $(function () {
-        $('input').iCheck({
-            checkboxClass: 'icheckbox_square-blue',
-            radioClass: 'iradio_square-blue',
-            increaseArea: '20%' // optional
-        });
-    });
-</script>
-
-</body>
-</html>
+@endsection
